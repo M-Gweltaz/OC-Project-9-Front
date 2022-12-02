ddfd8f2f70897cc9d1ccff9a424b707ac2db9d52
Gweltaz <gweltazmary@gmail.com>
2022-12-02 07:15:05 +0100

    Bug fix n4 handleEditTicket multiple call v2

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -95,7 +95,6 @@ export default class {
 	};
 
 	handleEditTicket(e, bill, bills) {
-		console.log('test');
 		if (this.counter === undefined || this.id !== bill.id) this.counter = 0;
 		if (this.id === undefined || this.id !== bill.id) this.id = bill.id;
 		if (this.counter % 2 === 0) {
@@ -141,7 +140,6 @@ export default class {
 	};
 
 	handleShowTickets(e, bills, index) {
-		console.log('test1');
 		if (this.counter === undefined || this.index !== index) this.counter = 0;
 		if (this.index === undefined || this.index !== index) this.index = index;
 		if (this.counter % 2 === 0) {
